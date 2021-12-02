db7e8c71059ae3b1bea323dee4152a6425886864
alvaro98ag <75319201+alvaro98ag@users.noreply.github.com>
2021-12-02 13:14:52 +0100

    Some changes

--- F16SimV2/OpenLoopAnalysis/OpenLoopAnalysis.m
+++ F16SimV2/OpenLoopAnalysis/OpenLoopAnalysis.m
@@ -1,10 +1,10 @@
 load("Trim_40000_300.mat");
 
-% LONGITUDINAL
+%% LONGITUDINAL
 A_OL = SS_long_lo.A(2:7,2:7); % 6x6
 B_OL = SS_long_lo.B(2:7,:); % 6x2
-save("A_OL.mat","A_OL");
-save("B_OL.mat","B_OL");
+save("A_OL_long.mat","A_OL");
+save("B_OL_long.mat","B_OL");
 
 % get A_ac
 A_temp = [SS_long_lo.A(3,:);SS_long_lo.A(4,:);SS_long_lo.A(2,:);SS_long_lo.A(5,:)];
@@ -17,5 +17,23 @@ A_overall = [A_ac B_ac;0 0 0 0 -a];
 B_overall = [0;0;0;0;a];
 C_overall = eye(5);
 D_overall = 0;
-system = ss(A_overall,B_overall,C_overall,D_overall);
-% LATERAL
+system_long = ss(A_overall,B_overall,C_overall,D_overall);
+save system_long.mat system_long
+
+%% LATERAL
+
+A_OL = SS_long_lo.A(2:7,2:7); % 6x6
+B_OL = SS_long_lo.B(2:7,:); % 6x2
+save("A_OL_lat.mat","A_OL");
+save("B_OL_lat.mat","B_OL");
+
+% get A_ac
+A_temp = [SS_long_lo.A(3,:);SS_long_lo.A(4,:);SS_long_lo.A(2,:);SS_long_lo.A(5,:)];
+A_ac = [A_temp(:,3) A_temp(:,4) A_temp(:,2) A_temp(:,5)];
+
+B_ac = [0;0;0;0];
+
+
+
+
+
