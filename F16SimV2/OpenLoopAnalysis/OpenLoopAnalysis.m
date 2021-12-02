b7d3ad84c52c0b90f685eca16f58f07dc4c2b119
alvaro98ag <75319201+alvaro98ag@users.noreply.github.com>
2021-12-02 13:59:14 +0100

    more changes

--- F16SimV2/OpenLoopAnalysis/OpenLoopAnalysis.m
+++ F16SimV2/OpenLoopAnalysis/OpenLoopAnalysis.m
@@ -22,18 +22,20 @@ save system_long.mat system_long
 
 %% LATERAL
 
-A_OL = SS_long_lo.A(2:7,2:7); % 6x6
-B_OL = SS_long_lo.B(2:7,:); % 6x2
-save("A_OL_lat.mat","A_OL");
-save("B_OL_lat.mat","B_OL");
+A_OL = [SS_lat_lo.A(4,:); SS_lat_lo.A(1,:); SS_lat_lo.A(5,:); SS_lat_lo.A(6,:)];
+A_ac = [A_OL(:,4) A_OL(:,1) A_OL(:,5) A_OL(:,6)]; %4x4             
+B_ac = zeros(4,2); % 4x2
 
-% get A_ac
-A_temp = [SS_long_lo.A(3,:);SS_long_lo.A(4,:);SS_long_lo.A(2,:);SS_long_lo.A(5,:)];
-A_ac = [A_temp(:,3) A_temp(:,4) A_temp(:,2) A_temp(:,5)];
+a = 20.2;
+b = 20.2;
 
-B_ac = [0;0;0;0];
+A_overall = [A_ac B_ac;0 0 0 0 -a 0; 0 0 0 0 0 -b];
+B_overall = [0 0;0 0;0 0;0 0;a 0; 0 b];
+C_overall = eye(6);
+D_overall = zeros(6,2);
 
-
+system_lat = ss(A_overall,B_overall,C_overall,D_overall);
+save system_lat.mat system_lat
 
 
 
