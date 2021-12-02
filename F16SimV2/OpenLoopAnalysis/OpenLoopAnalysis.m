b2dfa389d23dff919f2fa2d635eb8094ea1d2f5c
dirkdefuijk <60960313+dirkdefuijk@users.noreply.github.com>
2021-12-02 13:02:22 +0100

    Open loop analysis

---
+++ F16SimV2/OpenLoopAnalysis/OpenLoopAnalysis.m
@@ -0,0 +1,21 @@
+load("Trim_40000_300.mat");
+
+% LONGITUDINAL
+A_OL = SS_long_lo.A(2:7,2:7); % 6x6
+B_OL = SS_long_lo.B(2:7,:); % 6x2
+save("A_OL.mat","A_OL");
+save("B_OL.mat","B_OL");
+
+% get A_ac
+A_temp = [SS_long_lo.A(3,:);SS_long_lo.A(4,:);SS_long_lo.A(2,:);SS_long_lo.A(5,:)];
+A_ac = [A_temp(:,3) A_temp(:,4) A_temp(:,2) A_temp(:,5)];
+
+B_ac = [0;0;0;0];
+
+a = 20.2;
+A_overall = [A_ac B_ac;0 0 0 0 -a];
+B_overall = [0;0;0;0;a];
+C_overall = eye(5);
+D_overall = 0;
+system = ss(A_overall,B_overall,C_overall,D_overall);
+% LATERAL
--- F16SimV2/Vary_xa/plot_vary_xa.m
+++ F16SimV2/Vary_xa/plot_vary_xa.m
@@ -8,18 +8,18 @@ load('H_15.mat');
 
 opt = stepDataOptions("StepAmplitude",-1);
 figure
-xlim([0 10])
+xlim([0 3])
 step(H_elev_an_0,opt);
 hold on;
-xlim([0 10])
+xlim([0 3])
 step(H_elev_an_5,opt);
-xlim([0 10])
+xlim([0 3])
 step(H_elev_an_59,opt);
-xlim([0 10])
+xlim([0 3])
 step(H_elev_an_6,opt);
-xlim([0 10])
+xlim([0 3])
 step(H_elev_an_7,opt);
-xlim([0 10])
+xlim([0 3])
 step(H_elev_an_15,opt);
-xlim([0 10])
+xlim([0 3])
 legend("x_a = 0","x_a = 5","x_a = 5.9","x_a = 6","x_a = 7","x_a = 15")
