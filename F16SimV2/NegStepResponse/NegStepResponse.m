55d90a05c5c9bc549e1a9fa27eb523120433040e
dirkdefuijk <d.defuijk@gmail.com>
2022-02-06 12:33:54 +0100

    Cleaned NegStepResponse

--- F16SimV2/NegStepResponse/NegStepResponse.m
+++ F16SimV2/NegStepResponse/NegStepResponse.m
@@ -1,9 +1,11 @@
-load('Trim_indiv_xa0.mat');
+load('Trim_15000_500.mat');
 
 H_system = tf(SS_long_lo);
 H_elev_to_an = H_system(6,2); % Output 6 (a_n) due to input 2 (elevator)
-
+set(H_elev_to_an, "OutputName", "a_n");
+t = 0:0.001:2;
 opt = stepDataOptions("StepAmplitude", -1);
 figure
-step(H_elev_to_an,opt);
-xlim([0 2]);
+step(H_elev_to_an,opt,t);
+ylabel("a_n (m/s^2)")
+grid on
